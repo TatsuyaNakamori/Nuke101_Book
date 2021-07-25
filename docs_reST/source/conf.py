c53d4edde017bd719779052e2a4e59398dac1d05
TatsuyaNakamori <tnakamori.job.734@gmail.com>
2021-07-26 03:14:46 +0900

    編集中は、release を固定する

--- docs_reST/source/conf.py
+++ docs_reST/source/conf.py
@@ -27,7 +27,8 @@ version = '0.1'
 
 # The full version, including alpha/beta/rc tags
 import time
-release = time.strftime('%Y-%m-%d')
+# release = time.strftime('%Y-%m-%d')
+release = '0.1 - WIP'  # 編集中は固定にする
 
 
 # -- General configuration ---------------------------------------------------
